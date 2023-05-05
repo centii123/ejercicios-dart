f25030c5951ab666eb9497885d4404c63b3b58de
basganan <bas.ganan@yavirac.edu.ec>
2023-05-05 12:27:12 -0500

    g

---
+++ ejercciomate.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  print("Digite el valor de a: ");
+  int a = int.parse(stdin.readLineSync()!);
+  print("Digite el valor de b: ");
+  int b = int.parse(stdin.readLineSync()!);
+  print("Digite el valor de c: ");
+  int c = int.parse(stdin.readLineSync()!);
+}
