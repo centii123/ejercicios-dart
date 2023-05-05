94ecc4b00fa486d7827b0a3b47c6b3ccbaf83460
basganan <bas.ganan@yavirac.edu.ec>
2023-05-05 13:02:11 -0500

    g

--- ejercciomate.dart
+++ ejercciomate.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 void main() {
   print("Digite el valor de a: ");
@@ -7,4 +8,17 @@ void main() {
   int b = int.parse(stdin.readLineSync()!);
   print("Digite el valor de c: ");
   int c = int.parse(stdin.readLineSync()!);
+
+  int part2 = (b * b) - (4 * a * c);
+  int partDividir = 2 * a;
+
+  if (part2 <= 0) {
+    print("Lo sentimos a ocurrido un error");
+  } else {
+    double raiz = sqrt(part2);
+    double x1 = (-b + raiz) / partDividir;
+    double x2 = (-b - raiz) / partDividir;
+    print("x1= $x1");
+    print("x2= $x2");
+  }
 }
